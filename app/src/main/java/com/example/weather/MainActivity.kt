48e42e986276beda665f6baa08b6e25963c6091c
Manik <manbhatia007@gmail.com>
2022-11-10 00:33:46 +0530

    ALl done

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.weather
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -9,29 +10,62 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.RequestFuture
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val lat =intent.getStringExtra("lat")
-        val long =intent.getStringExtra("long")
+        val lat = intent.getStringExtra("lat")
+        val long = intent.getStringExtra("long")
+        window.statusBarColor = Color.parseColor("#1383C3")
         getJsonData(lat, long)
     }
 
-    private fun getJsonData(lat:String?,long:String?)
-    {
-        val API_Key="d782c3641dcc9cbcb8528dfb0245b3e4"
-          val queue =Volley.newRequestQueue(this)
-        val url="https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_Key}"
-        val jsonRequest=JsonObjectRequest(
-            Request.Method.GET,url,null,
-            Response.Listener{response ->
+    private fun getJsonData(lat: String?, long: String?) {
+        val API_Key = "d782c3641dcc9cbcb8528dfb0245b3e4"
+        val queue = Volley.newRequestQueue(this)
+        val url =
+            "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_Key}"
+        val jsonRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+                Setvalues(response)
 
             },
-        Response.ErrorListener{Toast.makeText(this,"ERROR ",Toast.LENGTH_LONG).show()})
+            Response.ErrorListener { Toast.makeText(this, "ERROR ", Toast.LENGTH_LONG).show() })
 
         queue.add(jsonRequest)
+
+
+    }
+
+    private fun Setvalues(response: JSONObject) {
+        city.text = response.getString("name")
+        var lat = response.getJSONObject("coord").getString("lat")
+        var long = response.getJSONObject("coord").getString("lon")
+        coordinates.text = "${lat},${long}"
+
+        weather.text = response.getJSONArray("weather").getJSONObject(0).getString("main")
+        var tempr = response.getJSONObject("main").getString("temp")
+        tempr = ((((tempr).toFloat() - 273.15)).toInt()).toString()
+        temp.text = "${tempr}C"
+
+        var mintemp = response.getJSONObject("main").getString("temp_min")
+        mintemp = ((((mintemp).toFloat() - 273.15)).toInt()).toString()
+        min_temp.text = mintemp + "C"
+
+        var maxtemp = response.getJSONObject("main").getString("temp_max")
+        maxtemp = ((ceil((maxtemp).toFloat() - 273.15)).toInt()).toString()
+        max_temp.text = maxtemp + "C"
+
+        pressure.text = response.getJSONObject("main").getString("pressure")
+        humidity.text = response.getJSONObject("main").getString("humidity") + "%"
+        wind.text = response.getJSONObject("wind").getString("speed")
+        degree.text = "Degree : " + response.getJSONObject("wind").getString("deg") + ""
+        // gust.text="Gust : "+response.getJSONObject("wind").getString("gust")+""
 
 
     }
